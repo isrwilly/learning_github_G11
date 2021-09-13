972e6c612bd221622a3edea723ac0207fc75e51b
Gonzalo Gamez <>
2021-09-13 09:53:27 +0300

    Adding what everone will do for assignment

---
+++ first_branches_and_merges.py
@@ -0,0 +1,37 @@
+#This is for group 2 and 3 to learn how to use github source control, branching and merging
+#I have specific challenge for each person
+'''Each person will:
+*clone the repo
+*make a branch named after themselves
+*use several commits to show progress on thier branch
+*test that your function works
+*add a file named after youself that you are proud to share
+*make a pull request for it to be merged to main branch
+'''
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
